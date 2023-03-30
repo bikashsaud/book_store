d555b8389cf8554b3aaee7ffa52062ad6db6e471
Bikash Saud <saudbikash514@gmail.com>
2023-03-30 08:35:13 +0545

    db operations for update and get book data

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/bikashsaud/book_store/pkg/config"
 	"github.com/jinzhu/gorm"
 )
@@ -21,7 +24,7 @@ func init() {
 }
 
 func (b *Book) CreateBook() *Book {
-	
+
 	db.NewRecord(b)
 	db.Create(&b)
 	return b
@@ -36,11 +39,44 @@ func GetBooks() []Book {
 func GetBook(id int64) (*Book, *gorm.DB) {
 	var getBook Book
 	db := db.Where("ID=?", id).Find(&getBook)
+	if db.RecordNotFound() {
+		fmt.Println("Book record not found.")
+	} else if db.Error != nil {
+		fmt.Println("Error occurred:", db.Error)
+	}
+
+	fmt.Println(db, 44444)
 	return &getBook, db
+
+}
+func GetBookById(id int64) (*Book, error) {
+	var book Book
+	result := db.Where("ID = ?", id).First(&book)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("Book with ID %d not found", id)
+		}
+		return nil, result.Error
+	}
+	return &book, nil
 }
 
 func DeleteBook(id int64) Book {
 	var book Book
 	db.Where("ID=?", id).Delete(book)
 	return book
+}
+
+func UpdateBook(id int64) Book {
+	var book Book
+	db.Where("ID=?", id).Update(book)
+	return book
+}
+
+func UpdateBookById(book *Book) error {
+	// update book in db
+	if err := db.Save(book).Error; err != nil {
+		return err
+	}
+	return nil
 }
